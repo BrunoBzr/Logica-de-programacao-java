8d2a8f8131e5b48b46e5e4bb2a9af828ec782409
Bruno Pereira <brunopereirabzrdev@gmail.com>
2025-01-26 11:31:25 -0300

    Update Main.java

--- CursoEmVideo/TiposPrimitivos/src/Main.java
+++ CursoEmVideo/TiposPrimitivos/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         float nota = 8.5f;
+        String nome = "Bruno";
         System.out.print("A nota é: " + nota);
+        System.out.println("\nA nota é: " + nota);
+        System.out.printf("A nota do %s é: %.1f", nome, nota);
     }
 }
