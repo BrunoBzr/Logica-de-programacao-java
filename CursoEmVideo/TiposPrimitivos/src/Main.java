ccde6d241159233b1fccab413fe9522aec53070d
Bruno Pereira <brunopereirabzrdev@gmail.com>
2025-01-26 11:38:13 -0300

    Entrada de dados

--- CursoEmVideo/TiposPrimitivos/src/Main.java
+++ CursoEmVideo/TiposPrimitivos/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        float nota = 8.5f;
-        String nome = "Bruno";
-        System.out.print("A nota é: " + nota);
-        System.out.println("\nA nota é: " + nota);
-        System.out.printf("A nota do %s é: %.1f", nome, nota);
+        Scanner teclado = new Scanner(System.in);
+        String nome;
+        float nota;
+        System.out.printf("Informe o nome do aluno: ");
+        nome = teclado.nextLine();
+        System.out.printf("Informe a nota: ");
+        nota = teclado.nextFloat();
+
+        System.out.format("A nota do %s é %.1f", nome, nota);
     }
 }
