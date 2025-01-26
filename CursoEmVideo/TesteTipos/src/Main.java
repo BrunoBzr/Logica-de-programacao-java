4470b5298257883e63818c6c12b54c98c45ddbf5
Bruno Pereira <brunopereirabzrdev@gmail.com>
2025-01-26 14:27:21 -0300

    Update

---
+++ CursoEmVideo/TesteTipos/src/Main.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Type;
+
+public class Main {
+    public static void main(String[] args) {
+        int idade = 23;
+        String valor = Integer.toString(idade);
+
+        String op = "12";
+        int op2 = Integer.parseInt(op);
+
+        int soma = idade +op2;
+        System.out.println(soma);
+
+    }
+}
